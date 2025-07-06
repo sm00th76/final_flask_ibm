df1294a65c2371d9df43526ab9f4132b8d4e09da
sm00th <khantwalbaladitya@gmail.com>
2025-07-06 04:16:11 -0400

    add emotion detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,12 @@
+import requests
+import json
+def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
+    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headerss
+    return response.text  # Return the response text from the API
+dict_response = json.loads(emotion_detector('i am so happy doing this'))
+
+print(dict_response)
+
